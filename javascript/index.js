895abcad2267358bee488eb55228e77a2d075f81
cardycode <135608773+cardycode@users.noreply.github.com>
2023-09-23 19:09:10 +0100

    Added select functionality js

--- javascript/index.js
+++ javascript/index.js
@@ -2,36 +2,62 @@ function updateTime() {
   //berlin
 
   let berlinElement = document.querySelector("#berlin");
-  let berlinDateElement = berlinElement.querySelector(".date");
-  let berlinTimeElement = berlinElement.querySelector(".time");
-  let berlinTime = moment().tz("Europe/Berlin");
-  berlinDateElement.innerHTML = berlinTime.format("dddd, MMMM Do YYYY");
-  berlinTimeElement.innerHTML = berlinTime.format(
-    "h:mm:ss[<span class='APM'>] A [</span>]"
-  );
-
+  if (berlinElement) {
+    let berlinDateElement = berlinElement.querySelector(".date");
+    let berlinTimeElement = berlinElement.querySelector(".time");
+    let berlinTime = moment().tz("Europe/Berlin");
+    berlinDateElement.innerHTML = berlinTime.format("dddd, MMMM Do YYYY");
+    berlinTimeElement.innerHTML = berlinTime.format(
+      "h:mm:ss[<span class='APM'>] A [</span>]"
+    );
+  }
   //sydney
 
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
-  sydneyDateElement.innerHTML = sydneyTime.format("dddd, MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "h:mm:ss[<span class='APM'>] A [</span>]"
-  );
-
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+    sydneyDateElement.innerHTML = sydneyTime.format("dddd, MMMM Do YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss[<span class='APM'>] A [</span>]"
+    );
+  }
   //vancouver
 
   let vancouverElement = document.querySelector("#vancouver");
-  let vancouverDateElement = vancouverElement.querySelector(".date");
-  let vancouverTimeElement = vancouverElement.querySelector(".time");
-  let vancouverTime = moment().tz("America/Vancouver");
-  vancouverDateElement.innerHTML = vancouverTime.format("dddd, MMMM Do YYYY");
-  vancouverTimeElement.innerHTML = vancouverTime.format(
-    "h:mm:ss[<span class='APM'>] A [</span>]"
-  );
+  if (vancouverElement) {
+    let vancouverDateElement = vancouverElement.querySelector(".date");
+    let vancouverTimeElement = vancouverElement.querySelector(".time");
+    let vancouverTime = moment().tz("America/Vancouver");
+    vancouverDateElement.innerHTML = vancouverTime.format("dddd, MMMM Do YYYY");
+    vancouverTimeElement.innerHTML = vancouverTime.format(
+      "h:mm:ss[<span class='APM'>] A [</span>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let timeZone = event.target.value;
+  let cityName = timeZone.split("/")[1];
+  let cityTime = moment().tz(timeZone);
+  let citiesElement = document.querySelector(".cities");
+  citiesElement.innerHTML = `<div
+          class="city"
+          id=""
+        >
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss[<span class='APM'>] A [</span>]"
+          )}</div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#citySelect");
+citySelectElement.addEventListener("change", updateCity);
